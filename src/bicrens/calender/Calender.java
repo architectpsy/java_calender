ea925875380f819caaf9149d81b6ba8fec82ff8c
User 2 <User 2@DESKTOP-J6KHOK5>
2018-10-05 18:27:27 +0900

    getMaxDaysOfMonth method 추가

--- src/bicrens/calender/Calender.java
+++ src/bicrens/calender/Calender.java
@@ -3,6 +3,12 @@ package bicrens.calender;
 import java.util.Scanner;
 
 public class Calender {
+	
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month-1];
+	}
 	public static void main(String[] args) {
 		System.out.println(" 일    월    화    수    목    금    토");
 		System.out.println("---------------------");
@@ -13,12 +19,12 @@ public class Calender {
 		
 		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
+		Calender cal = new Calender();
+		
 		System.out.println("달을 입력하세요");
 		int month = scanner.nextInt();
 		
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month-1]);
+		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
 		scanner.close();
 	}
 }
