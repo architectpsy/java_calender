8605083722463b854ad26a84e6e59c99e6f8aeea
User 2 <User 2@DESKTOP-J6KHOK5>
2018-10-08 11:15:59 +0900

    반복 기능 추가

--- src/bicrens/calender/Calender.java
+++ src/bicrens/calender/Calender.java
@@ -21,15 +21,22 @@ public class Calender {
 	
 	public static void main(String[] args) {
 		
-		
-		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
 		Calender cal = new Calender();
 		
-		System.out.println("달을 입력하세요");
-		int month = scanner.nextInt();
+		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		System.out.println("반복횟수를 입력하세요.");
+		int repeat = scanner.nextInt();
 		
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		
+		
+		for (int i = 0; i<repeat; i++) {
+			System.out.println("달을 입력하세요");
+			int month = scanner.nextInt();
+			
+			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		}
+		
 		scanner.close();
 	}
 }
